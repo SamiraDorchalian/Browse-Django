e0d44f506a5b27adf9941253046204f0b4b2b192
samira dorchalian <samiradorchalain1998@gmail.com>
2025-05-26 19:18:23 +0330

    change some field in models

--- store/store/models.py
+++ store/store/models.py
@@ -1,13 +1,27 @@
 from django.db import models
 
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=500, blank=True)
+    top_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
+
+
+class Discount(models.Model):
+    discount = models.FloatField()
+    description = models.CharField(max_length=255)
+
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='products')
     slug = models.SlugField()
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     datetime_create = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
+    discounts = models.ManyToManyField(Discount, blank=True)
 
 
 class Customer(models.Model):
@@ -15,4 +29,71 @@ class Customer(models.Model):
     last_name = models.CharField(max_length=255)
     email = models.EmailField()
     phone_number = models.CharField(max_length=255)
-    birth_date = models.DateField()
+    birth_date = models.DateField(null=True, blank=True)
+    # fathers_name = models.CharField(max_length=255)
+
+
+class Address(models.Model):
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
+    province = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    street = models.CharField(max_length=255)
+
+    # class Meta:
+    #     db_table = 'customer_address'
+
+
+class Order(models.Model):
+    ORDER_STATUS_PAID = 'p'
+    ORDER_STATUS_UNPAID = 'u'
+    ORDER_STATUS_CANCELED = 'c'
+    ORDER_STATUS = [
+        (ORDER_STATUS_PAID,'Paid'),
+        (ORDER_STATUS_UNPAID,'Unpaid'),
+        (ORDER_STATUS_CANCELED,'Canceled'),
+    ]
+    
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT, related_name='orders')
+    datetime_create = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=1, choices=ORDER_STATUS, default=ORDER_STATUS_UNPAID)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='order_items')
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    class Meta:
+        unique_together = [['order', 'product']]
+
+
+class Comment(models.Model):
+    COMMENT_STATUS_WAITING = 'w'
+    COMMENT_STATUS_APPROVED = 'a'
+    COMMENT_STATUS_NOT_APPROVED = 'na'
+
+    COMMENT_STATUS = [
+        (COMMENT_STATUS_WAITING, 'Waiting'),
+        (COMMENT_STATUS_APPROVED, 'Approved'),
+        (COMMENT_STATUS_NOT_APPROVED, 'Not Approved'),
+    ]
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=255)
+    body = models.TextField()
+    datetime_create = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=2, choices=COMMENT_STATUS, default=COMMENT_STATUS_NOT_APPROVED)
+
+
+class Cart(models.Model):
+    create_at = models.DateTimeField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='cart_items')
+    quantity = models.PositiveSmallIntegerField()
+
+    class Meta:
+        unique_together = [['cart', 'product']]
