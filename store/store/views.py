2b91a21032d3b9aea3b7cd18b11d52ab4cc7fe21
samira dorchalian <samiradorchalain1998@gmail.com>
2025-05-28 17:03:22 +0330

    annotate

--- store/store/views.py
+++ store/store/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpRequest
 from decimal import Decimal
-from django.db.models import Q, F, Count, Min, Max, Sum, Avg
+from django.db.models import Q, F, Count, Min, Max, Sum, Avg , Value, Func
 # from django.db.models.aggregates import Count
 
 from .models import Product, Customer, OrderItem, Order, Comment
@@ -28,9 +28,12 @@ def show_data(request):
 #       queryset = Product.objects.filter(inventory__gt=90).order_by('unit_price')[0]
 #       queryset = Product.objects.filter(inventory__gt=90).earliest('-unit_price')
       # queryset = Product.objects.filter(inventory__gt=90).latest('-unit_price')
+      
+      # values & values_list & only & defer
+      
       # queryset = Product.objects.values('id', 'name', 'unit_price')
       # queryset = Product.objects.order_by('-inventory').values('name', 'inventory')
-      # queryset = OrderItem.objects.values('product_id').distinct()
+      # queryset = OrderItem.objects.values('product_id').distinct() #ردیف‌های تکراری رو از نتایج کوئری حذف میکنه
       # queryset_orderitems_products = OrderItem.objects.values('product_id').distinct()
       # queryset = Product.objects.filter(id__in=queryset_orderitems_products)
       # queryset = Product.objects.filter(id__in=OrderItem.objects.values('product_id').distinct())
@@ -38,12 +41,18 @@ def show_data(request):
       # queryset = Product.objects.only('id', 'name', 'inventory') # چه چیز های رو فقط میخام
       # queryset = Product.objects.values('id', 'name') 
       # queryset = Product.objects.defer('unit_price') # چه چیز هایی رو نمیخام
+
+      # select_related & prefetch_related
+
       # queryset = OrderItem.objects.select_related('product').all() # مربوط به فیلدی که ForeignKey خورده .
       # queryset = Product.objects.prefetch_related('order_items').all()  # برعکس بالایی عمل میکنه مثلا متد پروداکت در فایل مدل در کدوم اوردرایتم ها استفاده شده 
       # queryset = Comment.objects.select_related('product').all()
       # queryset = Product.objects.prefetch_related('comments').all()
       # queryset = Product.objects.select_related('category').prefetch_related('comments').all()
       # queryset = Order.objects.prefetch_related('items__product').select_related('customer').all()
+
+      # aggregate & Count & Avg ...
+
       # queryset = Product.objects.aggregate(Count('id'))
       # queryset = Comment.objects.aggregate(Count('id'))
       # queryset = OrderItem.objects.aggregate(Count('id'))
@@ -61,8 +70,19 @@ def show_data(request):
       # queryset = OrderItem.objects.filter(product_id=2).aggregate(Count('id'))
       # product = Product.objects.get(id=1)
       # print(product.order_items.aggregate(count=Count('id')))
-      queryset = OrderItem.objects.values('product_id').distinct()
-      print(queryset.count())
+      # queryset = OrderItem.objects.values('product_id').distinct() 
+
+      # annotate & Value & F & Func
+ 
+      # queryset = Product.objects.annotate(x=Value(1)).all()[:2] # تعریف ستون جدید بر اساس اون چیزی که ما میخوایم مثل ستون ایکس که تعریف کردیم (مقادیر محاسبه‌شده (مثل جمع، میانگین، یا شمارش) رو به هر شیء تو نتایج کوئری اضافه کنه)
+      # queryset = Product.objects.annotate(price=F('unit_price')).all()[:2] # F() یه ابزار قدرتمند تو جنگوئه که برای کار با مقادیر فیلدهای مدل تو سطح دیتابیس استفاده می‌شه، بدون اینکه داده‌ها رو تو حافظه پایتون بارگذاری کنه. این برای به‌روزرسانی‌ها، مقایسه‌ها و محاسبات مستقیم تو دیتابیس عالیه. F = Field
+      # queryset = OrderItem.objects.annotate(total_price=F('quantity') * F('unit_price')).all()
+      # queryset = Customer.objects.annotate(fullname=Func(F('first_name'), Value(' '), F('last_name'), function='CONCAT')).defer('first_name', 'last_name')
+
+      # group by
+      
+      queryset = Customer.objects.annotate(fullname=Func(F('first_name'), Value(' '), F('last_name'), function='CONCAT')).defer('first_name', 'last_name')
+      print(queryset)
       return render(request, 'hello.html' )
       # print(list(queryset))
 #       print([x.unit_price for x in list(queryset)])
