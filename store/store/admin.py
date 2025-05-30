b1a7c923169edba17c496b6ce579fe71077de812
samira dorchalian <samiradorchalain1998@gmail.com>
2025-05-30 14:43:29 +0330

    Computed Fields

--- store/store/admin.py
+++ store/store/admin.py
@@ -4,10 +4,25 @@ from .models import Category, Order, Product
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'inventory', 'unit_price', 'datetime_create',]
+    # list_display = ['id', 'name', 'inventory', 'unit_price', 'is_low', ]
+    list_display = ['id', 'name', 'inventory', 'unit_price', 'inventory_status', ]
     list_per_page = 10
     list_editable = ['unit_price']
-    ordering = ['-datetime_create']
+    # ordering = ['-datetime_create']
+
+    # Computed Fields
+
+    # def is_low(self, product):
+    #     return product.inventory < 10
+    
+    # def inventory_status(self, product):
+    def inventory_status(self, product: Product):
+        if product.inventory < 10:
+            return 'Low'
+        if product.inventory > 50:
+            return 'High'
+        return 'Medium'
+
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
