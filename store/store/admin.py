94961ff11e4cc9ed60f7ba9ef80b7cf457f28ba1
samira dorchalian <samiradorchalain1998@gmail.com>
2025-05-30 15:03:18 +0330

    useing from select related on ListAdmin

--- store/store/admin.py
+++ store/store/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 
-from .models import Category, Order, Product
+from .models import Category, Order, Product ,Comment
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     # list_display = ['id', 'name', 'inventory', 'unit_price', 'is_low', ]
-    list_display = ['id', 'name', 'inventory', 'unit_price', 'inventory_status', ]
+    list_display = ['id', 'name', 'inventory', 'unit_price', 'inventory_status', 'product_category' ]
     list_per_page = 10
     list_editable = ['unit_price']
     # ordering = ['-datetime_create']
+    list_select_related = ['category']
 
     # Computed Fields
 
@@ -16,12 +17,19 @@ class ProductAdmin(admin.ModelAdmin):
     #     return product.inventory < 10
     
     # def inventory_status(self, product):
-    def inventory_status(self, product: Product):
+    def inventory_status(self, product: Product): #product: Product for suggestion by VSCode
         if product.inventory < 10:
             return 'Low'
         if product.inventory > 50:
             return 'High'
         return 'Medium'
+    
+    # select related in ListAdmin
+
+    # @admin.display(ordering='category_id') # for sort default in panel admin
+    @admin.display(ordering='category__title') # for sort default in panel admin
+    def product_category(self, product):
+        return product.category.title
 
 
 @admin.register(Order)
@@ -30,6 +38,13 @@ class OrderAdmin(admin.ModelAdmin):
     list_editable = ['status']
     list_per_page = 10
     ordering = ['-datetime_create']
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'product', 'status', ]
+    list_editable = ['status']
+    list_per_page = 10
+
 
 # admin.site.register(Product, ProductAdmin)
 admin.site.register(Category)
