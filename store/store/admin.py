75ae76ae5df64ff4d906e33da87a75e31e99b72e
samira dorchalian <samiradorchalain1998@gmail.com>
2025-05-30 15:59:45 +0330

    Search Fields

--- store/store/admin.py
+++ store/store/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.db.models import Count
 
-from .models import Category, Order, Product ,Comment
+from .models import Category, Customer, Order, Product ,Comment
 
 # Custom Filtering
 class InventoryFilter(admin.SimpleListFilter):
@@ -89,6 +89,17 @@ class OrderAdmin(admin.ModelAdmin):
     def num_of_items(self, order): # by related_name='orders' in file models / ForeignKey
         # return order.items.count()
         return order.items_count
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'email',]
+    list_per_page = 10
+    ordering = ['last_name', 'first_name', ]
+# Search Fields / add Search box in panelAdmin
+    # search_fields = ['first_name', 'last_name', ]
+    # search_fields = ['first_name__startswith', 'last_name__startswith', ] # What word should it start with? & Case sensitive
+    search_fields = ['first_name__istartswith', 'last_name__istartswith', ] # its not Case sensitive => istartswith
 
 
 # admin.site.register(Product, ProductAdmin)
