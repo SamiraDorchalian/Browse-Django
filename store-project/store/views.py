0a81b48a6c57613a493b055b61f42634df9b5421
samira dorchalian <samiradorchalain1998@gmail.com>
2025-05-31 14:53:37 +0330

    How to get and display information with API

--- store-project/store/views.py
+++ store-project/store/views.py
@@ -1,5 +1,11 @@
+from django.shortcuts import get_object_or_404
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Product
+from .serializers import ProductSerializers
 
 @api_view()
 def product_list(request):
@@ -7,4 +13,11 @@ def product_list(request):
 
 @api_view()
 def product_detail(request, id):
-    return Response(id)
+    product = get_object_or_404(Product, pk=id)
+    # try:
+    #     product = Product.objects.get(pk=id) #get data from database
+    # except Product.DoesNotExist:
+    #     return Response(status=status.HTTP_404_NOT_FOUND) #If there was no data
+
+    serializer = ProductSerializers(product) # Data processing to change format
+    return Response(serializer.data) # Deliver data to the user in JSON format
