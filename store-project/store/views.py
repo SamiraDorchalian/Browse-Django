0fd566f32fbf100b33ee9bba41d8b6327d77ded7
samira dorchalian <samiradorchalain1998@gmail.com>
2025-05-31 14:26:36 +0330

    intsall django-rest_framework - using browsable api

--- store-project/store/views.py
+++ store-project/store/views.py
@@ -1,2 +1,10 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+@api_view()
+def product_list(request):
+    return Response('Hello')
+
+@api_view()
+def product_detail(request, id):
+    return Response(id)
