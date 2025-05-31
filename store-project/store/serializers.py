caafc36260e6043a645368fe18880b4a3696d4c7
samira dorchalian <samiradorchalain1998@gmail.com>
2025-05-31 16:36:12 +0330

    using from hyperlink related field

--- store-project/store/serializers.py
+++ store-project/store/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 
 from .models import Category
 
-class CategorySerializers(serializers.Serializer):
+class CategorySerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
     description = serializers.CharField(max_length=500)
 
@@ -14,7 +14,11 @@ class ProductSerializers(serializers.Serializer):
     unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
     unit_price_after_tax = serializers.SerializerMethodField()
     inventory = serializers.IntegerField()
-    category = CategorySerializers()
+    # category = CategorySerializer()
+    category = serializers.HyperlinkedRelatedField(
+        queryset=Category.objects.all(),
+        view_name='category-detail',
+    )
 
     def get_unit_price_after_tax(self, product):
         return round(product.unit_price * Decimal(1.09), 2)
