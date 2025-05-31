9ce4756da82db2d18905bfced05aca65f84abff1
samira dorchalian <samiradorchalain1998@gmail.com>
2025-05-31 18:04:14 +0330

    serializer create method change

--- store-project/store/serializers.py
+++ store-project/store/serializers.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+from django.utils.text import slugify
 from rest_framework import serializers
 
 from .models import Category, Product
@@ -15,7 +16,7 @@ class ProductSerializers(serializers.ModelSerializer):
 
     class Meta:
         model = Product
-        fields = ['id', 'title',  'price', 'category','unit_price_after_tax', 'inventory', 'slug', 'description']
+        fields = ['id', 'title',  'price', 'category','unit_price_after_tax', 'inventory', 'description']
 
     def get_unit_price_after_tax(self, product):
         return round(product.unit_price * Decimal(1.09), 2)
@@ -24,3 +25,9 @@ class ProductSerializers(serializers.ModelSerializer):
         if len(data['name']) < 6:
             raise serializers.ValidationError('Product title length should be at least 6.')
         return data
+    
+    def create(self, validated_data):
+        product = Product(**validated_data)
+        product.slug = slugify(product.name)
+        product.save()
+        return product
