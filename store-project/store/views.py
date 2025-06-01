e8bcc05204f82509d97ee0ef649bbea5d011d0ef
samira dorchalian <samiradorchalain1998@gmail.com>
2025-06-01 20:28:47 +0330

    Calculating the total price of shopping cart items and optimizing its query

--- store-project/store/views.py
+++ store-project/store/views.py
@@ -70,4 +70,4 @@ class CartViewSet(CreateModelMixin,
                    RetrieveModelMixin,
                    GenericViewSet):
     serializer_class = CartSerializer
-    queryset = Cart.objects.all()
+    queryset = Cart.objects.prefetch_related('items__product').all()
