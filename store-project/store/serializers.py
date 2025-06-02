3de87b8e1b305610bc50d5f8c28282084dd502be
samira dorchalian <samiradorchalain1998@gmail.com>
2025-06-02 16:54:05 +0330

    Request of type put for views actions

--- store-project/store/serializers.py
+++ store-project/store/serializers.py
@@ -113,4 +113,5 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = ['id', 'user', 'birth_date']
+        read_only_fields = ['user']
 
