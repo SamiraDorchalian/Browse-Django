2d4fede0af021ccc269ca36097973fcf4e2f181c
samira dorchalian <samiradorchalain1998@gmail.com>
2025-06-02 15:19:47 +0330

    Django Djoser Package

--- store-project/config/urls.py
+++ store-project/config/urls.py
@@ -23,6 +23,8 @@ admin.site.index_title = 'Special Access'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('store/', include('store.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
 
